14fd4cf076db1101eb607c1d1d0b1034be4043fa
Andre <andremedeiros.aluno@unipampa.edu.br>
2024-06-20 03:52:02 -0300

    add arb languages, reduze language cover

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -2,106 +2,23 @@ import 'package:flutter/material.dart';
 
 class L10n {
   static final all = [
-    const Locale('af'), // Afrikaans
-    const Locale('am'), // Amárico
     const Locale('ar'), // Árabe
-    const Locale('az'), // Azerbaijano
-    const Locale('be'), // Bielorrusso
-    const Locale('bg'), // Búlgaro
     const Locale('bn'), // Bengali
-    const Locale('bs'), // Bósnio
-    const Locale('ca'), // Catalão
-    const Locale('cs'), // Tcheco
-    const Locale('cy'), // Galês
-    const Locale('da'), // Dinamarquês
     const Locale('de'), // Alemão
-    const Locale('el'), // Grego
     const Locale('en'), // Inglês
-    const Locale('eo'), // Esperanto
     const Locale('es'), // Espanhol
-    const Locale('et'), // Estoniano
-    const Locale('eu'), // Basco
-    const Locale('fa'), // Persa (Farsi)
-    const Locale('fi'), // Finlandês
-    const Locale('fil'), // Filipino
     const Locale('fr'), // Francês
-    const Locale('ga'), // Irlandês
-    const Locale('gl'), // Galego
-    const Locale('gu'), // Gujarati
-    const Locale('he'), // Hebraico
     const Locale('hi'), // Hindi
-    const Locale('hr'), // Croata
-    const Locale('ht'), // Crioulo haitiano
-    const Locale('hu'), // Húngaro
-    const Locale('hy'), // Armênio
     const Locale('id'), // Indonésio
-    const Locale('is'), // Islandês
     const Locale('it'), // Italiano
     const Locale('ja'), // Japonês
-    const Locale('ka'), // Georgiano
-    const Locale('kk'), // Cazaque
-    const Locale('km'), // Khmer
-    const Locale('kn'), // Canarês
     const Locale('ko'), // Coreano
-    const Locale('ku'), // Curdo
-    const Locale('ky'), // Quirguiz
-    const Locale('la'), // Latim
-    const Locale('lb'), // Luxemburguês
-    const Locale('lo'), // Lao
-    const Locale('lt'), // Lituano
-    const Locale('lv'), // Letão
-    const Locale('mg'), // Malgaxe
-    const Locale('mk'), // Macedônio
-    const Locale('ml'), // Malaiala
-    const Locale('mn'), // Mongol
-    const Locale('mr'), // Marata
     const Locale('ms'), // Malaio
-    const Locale('mt'), // Maltês
-    const Locale('my'), // Birmanês
-    const Locale('ne'), // Nepalês
-    const Locale('nl'), // Holandês
-    const Locale('no'), // Norueguês
-    const Locale('ny'), // Nianja
-    const Locale('or'), // Oriá
-    const Locale('pa'), // Punjabi
-    const Locale('pl'), // Polonês
-    const Locale('ps'), // Pashto
     const Locale('pt', 'BR'), // Português (Brasil)
-    const Locale('pt', 'PT'), // Português (Portugal)
-    const Locale('ro'), // Romeno
     const Locale('ru'), // Russo
-    const Locale('rw'), // Kinyarwanda
-    const Locale('sd'), // Sindhi
-    const Locale('si'), // Cingalês
-    const Locale('sk'), // Eslovaco
-    const Locale('sl'), // Esloveno
-    const Locale('sm'), // Samoano
-    const Locale('sn'), // Xona
-    const Locale('so'), // Somali
-    const Locale('sq'), // Albanês
-    const Locale('sr'), // Sérvio
-    const Locale('st'), // Sesotho
-    const Locale('su'), // Sunda
-    const Locale('sv'), // Sueco
-    const Locale('sw'), // Suaíli
-    const Locale('ta'), // Tâmil
-    const Locale('te'), // Telugu
-    const Locale('tg'), // Tajique
     const Locale('th'), // Tailandês
-    const Locale('ti'), // Tigrínia
-    const Locale('tk'), // Turcomano
-    const Locale('tl'), // Tagalo
     const Locale('tr'), // Turco
-    const Locale('tt'), // Tatar
-    const Locale('ug'), // Uigur
-    const Locale('uk'), // Ucraniano
-    const Locale('ur'), // Urdu
-    const Locale('uz'), // Usbeque
     const Locale('vi'), // Vietnamita
-    const Locale('xh'), // Xhosa
-    const Locale('yi'), // Yiddish
-    const Locale('yo'), // Iorubá
     const Locale('zh'), // Chinês (simplificado)
-    const Locale('zu'), // Zulu
   ];
 }
